14ecfdab9c013037017f4c760c370317f0f505f8
james-healey <james@carbonrmp.com>
2021-02-14 20:29:57 +0000

    Build out more of the testing

--- tests/SimpleScraperTests/Tests.cs
+++ tests/SimpleScraperTests/Tests.cs
@@ -9,6 +9,8 @@ namespace SimpleScraperTests
     public class Tests
     {
         private static readonly string HomepageHtml = File.ReadAllText("resources/Monzo – Banking made easy.html");
+        private static readonly string[] AllLinks = File.ReadAllLines("resources/all-links.txt");
+        private static readonly string[] MonzoLinks = File.ReadAllLines("resources/same-domain-links.txt");
 
         [TestMethod]
         public void Get_Html_From_Url()
@@ -23,55 +25,38 @@ namespace SimpleScraperTests
         [TestMethod]
         public void Get_Links_From_Single_Page()
         {
+            var expected = AllLinks;
             var result = new LinkExtractor().Extract(HomepageHtml);
-            var expected = new List<string>()
-            {
-                "https://monzo.com",
-                ""
-            };
+
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
         public void Get_Same_Domain_Links_From_Single_Page()
         {
+            var expected = MonzoLinks;
             var result = new SameDomainLinkExtractor().Extract(HomepageHtml);
-            var expected = new List<string>()
-            {
-                "https://monzo.com",
-                ""
-            };
+
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
-        public void Get_HTML_Links_From_Single_Page()
-        {
-            var result = new HtmlLinkExtractor().Extract(HomepageHtml);
-            var expected = new List<string>()
-            {
-                "https://monzo.com",
-                ""
-            };
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void Scrape_Site()
+        public void Scrape_Url()
         {
             var url = "https://monzo.com";
             var result = new SimpleScraper.SimpleScraper().Scrape(url);
-            var expected = new Dictionary<string, List<string>>
+            var expected = new Dictionary<string, string[]>
             {
                 {
-                    "https://monzo.com",
-                    new List<string>()
-                    {
-                        "",
-                        "",
-                        ""
-                    }
-                }
+                    "https://monzo.com", MonzoLinks
+                },
+                {
+                    "https://monzo.com/about", new string[] { "TODO" }
+                },
+                {
+                    "https://monzo.com/blog/", new string[] { "TODO" }
+                },
+                //.... TODO
             };
             Assert.AreEqual(expected, result);
         }
